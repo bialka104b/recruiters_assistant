0870519f3b2334be854bd3abf5fd215d09aab2df
Marta Kulig <bialka104b@interia.pl>
2022-06-18 23:38:48 +0200

    dodano paczke multiselect

--- frontend/recruiters_assistant/src/shims-vuetify.d.ts
+++ frontend/recruiters_assistant/src/shims-vuetify.d.ts
@@ -1,4 +1,9 @@
 declare module 'vuetify'
 declare module 'vuetify/lib/components'
 declare module 'vuetify/lib/directives'
-declare module '*.vue';
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+declare module 'vue-multiselect'
