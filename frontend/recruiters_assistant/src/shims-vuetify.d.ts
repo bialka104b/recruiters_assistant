4493b09db3bbfc80471e4c2a71974634b5699c68
Marta Kulig <bialka104b@interia.pl>
2022-06-19 23:06:30 +0200

    dodano favicon, usunięto niepotrzebne pliki vuetify

--- frontend/recruiters_assistant/src/shims-vuetify.d.ts
+++ frontend/recruiters_assistant/src/shims-vuetify.d.ts
@@ -1,6 +1,3 @@
-declare module 'vuetify'
-declare module 'vuetify/lib/components'
-declare module 'vuetify/lib/directives'
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
